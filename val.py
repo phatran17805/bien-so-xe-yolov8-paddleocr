573dcf750cd43ebabc14634e707596ba19f541df
phatran17805 <phatran17805@gmail.com>
2026-08-01 16:19:42 +0700

    Update val.py

--- val.py
+++ val.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # Load mô hình best
-    model = YOLO('runs/detect/traffic_model/weights/best.pt')
+    model = YOLO('weights/best.pt')
 
     # Đánh giá trên tập test
     metrics = model.val(data='data.yaml', split='test')
